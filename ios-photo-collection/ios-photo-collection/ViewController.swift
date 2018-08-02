bf9c73bb77f0c3088ca11d4afaf6825298102410
Conner Alegre <cga0@pm.me>
2018-08-02 13:12:35 -0600

    Initial commit

---
+++ ios-photo-collection/ios-photo-collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-photo-collection
+//
+//  Created by Conner on 8/2/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
