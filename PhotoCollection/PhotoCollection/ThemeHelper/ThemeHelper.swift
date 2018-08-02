43cc03823c1dba2135a6e338887e65a093cc49d6
Linh Bouniol <paulalinh89@gmail.com>
2018-08-02 12:46:37 -0700

    Created a theme helper

---
+++ PhotoCollection/PhotoCollection/ThemeHelper/ThemeHelper.swift
@@ -0,0 +1,32 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by Linh Bouniol on 8/2/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    
+    let themePreferenceKey = "themePreference"
+    
+    var themePreference: String? {
+        return UserDefaults.standard.string(forKey: themePreferenceKey)
+    }
+    
+    func setThemePreferenceToDark() {
+        UserDefaults.standard.set("Dark", forKey: themePreferenceKey)
+    }
+    
+    func setThemepreferenceToYourColorHere() {
+        UserDefaults.standard.set("Red", forKey: themePreferenceKey)
+    }
+    
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToDark()
+        }
+    }
+}
