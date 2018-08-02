96db268bd43ae44cf0a1e151574c94f3d317785a
Linh Bouniol <paulalinh89@gmail.com>
2018-08-02 14:42:29 -0700

    Set up detail view controller to save photos and update views

--- PhotoCollection/PhotoCollection/View Controllers/PhotoDetailViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoDetailViewController.swift
@@ -21,13 +21,42 @@ class PhotoDetailViewController: UIViewController {
     }
     
     @IBAction func savePhoto(_ sender: Any) {
+        // Make sure everything exists
+        guard let title = textField.text, let image = imageView.image, let imageData = UIImagePNGRepresentation(image) else { return }
+        
+        if let photo = photo {
+            photoController?.update(photo: photo, title: title, imageData: imageData)
+        } else {
+            photoController?.createPhoto(withTitle: title, imageData: imageData)
+        }
+        
+        navigationController?.popViewController(animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
 
-
+    func setTheme() {
+        guard let themePreference = themeHelper?.themePreference else { return }
+        
+        if themePreference == "Red" {
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.darkGray
+        }
+    }
+    
+    func updateViews() {
+        if photo == nil {
+            navigationItem.title = "Add Photo"
+        } else {
+            guard let photo = photo else { return }
+            navigationItem.title = "Edit Photo"
+            imageView?.image = UIImage(data: photo.imageData)
+            textField?.text = photo.title
+        }
+    }
 }
