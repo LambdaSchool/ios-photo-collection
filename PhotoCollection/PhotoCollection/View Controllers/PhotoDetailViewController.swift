c8d4cb3b5930442e8aae2830b18dddb763d7ed3e
Lisa Sampson <lisasampson0918@gmail.com>
2018-08-09 20:13:40 -0400

    Complete all connections in app with Detail View Controller

--- PhotoCollection/PhotoCollection/View Controllers/PhotoDetailViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoDetailViewController.swift
@@ -8,40 +8,78 @@
 
 import UIKit
 
-class PhotoDetailViewController: UIViewController {
+class PhotoDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+        setTheme()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func addPhoto(_ sender: Any) {
-        
+        presentImagePickerController()
     }
     
     @IBAction func savePhoto(_ sender: Any) {
+        guard let title = textField.text,
+            let image = imageView.image,
+            let imageData = UIImagePNGRepresentation(image) else { return }
         
+        if let photo = photo {
+            photoController?.update(photo: photo, imageData: imageData, title: title)
+        } else {
+            photoController?.create(imageData: imageData, title: title)
+        }
+        navigationController?.popViewController(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setTheme() {
+        guard let themePreference = themeHelper?.themePreference else { return }
+        
+        if themePreference == "Purple" {
+            self.view.backgroundColor = UIColor.purple
+        } else {
+            self.view.backgroundColor = UIColor.darkGray
+        }
     }
-    */
+    
+    func updateViews() {
+        if let photo = photo {
+            navigationItem.title = "Edit Photo"
+            imageView?.image = UIImage(data: photo.imageData)
+            textField?.text = photo.title
+        } else {
+            navigationItem.title = "Add Photo"
+        }
+    }
+    
+    func presentImagePickerController() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        imageView.image = image
+    }
     
     var photoController: PhotoController?
-    var photo: Photo?
-    var themeHelper: ThemeHelper?
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    var themeHelper: ThemeHelper? {
+        didSet {
+            setTheme()
+        }
+    }
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textField: UITextField!
