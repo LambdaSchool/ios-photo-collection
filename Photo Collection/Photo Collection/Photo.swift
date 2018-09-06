b8a4e497cc086ad8c18d1d0d23780ff5e2bc47fc
Daniela Parra <dyparra47@gmail.com>
2018-09-06 17:06:46 -0400

    Set up model and model controllers.

---
+++ Photo Collection/Photo Collection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Daniela Parra on 9/6/18.
+//  Copyright © 2018 Daniela Parra. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
