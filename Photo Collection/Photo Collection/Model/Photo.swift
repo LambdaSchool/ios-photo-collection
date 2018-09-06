102db6d1b657344687bb60e33c68a9da08bfc68e
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-06 13:23:07 -0600

    Add photo model and model controller

---
+++ Photo Collection/Photo Collection/Model/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Dillon McElhinney on 9/6/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
---
+++ Photo Collection/Photo Collection/Model/PhotoController.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoController.swift
+//  Photo Collection
+//
+//  Created by Dillon McElhinney on 9/6/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import Foundation
+
+class PhotoController {
+    
+    // MARK: - Properties
+    private(set) var photos: [Photo] = []
+    
+    // MARK: - CRUD Methods
+    // Create a new photo and add it to the array
+    func createPhoto(title: String, imageData: Data) {
+        let photo = Photo(imageData: imageData, title: title)
+        
+        photos.append(photo)
+    }
+    
+    // Update an existing photo in the array
+    func update(photo: Photo, title: String, imageData: Data) {
+        guard let index = photos.index(of: photo) else { return }
+        
+        photos[index].title = title
+        photos[index].imageData = imageData
+    }
+}
