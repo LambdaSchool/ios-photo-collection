8603eccc27af9c400440b3f0f591fb4bc43711b8
Rwoltx <richardx1016@gmail.com>
2018-09-13 16:53:33 -0400

    1st commit.

---
+++ PhotoCollection/PhotoCollection/Model/Photo.swift
@@ -0,0 +1,17 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Rick Wolter on 9/13/18.
+//  Copyright © 2018 RNWolter. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    
+    let imageData: Data
+    let title: String
+    
+    
+}
