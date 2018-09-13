4cc4bd5c38d53bb7aa102059b384ad0100b7927e
iyinraphael <iyinraphael@gmail.com>
2018-09-13 13:22:21 -0700

    created model and model controller

--- Photo Collection/Photo Collection/Model Controller/PhotoController.swift
+++ Photo Collection/Photo Collection/Model Controller/PhotoController.swift
@@ -10,5 +10,19 @@ import Foundation
 
 class PhotoController{
     
+    func createPhoto(title: String, imageData: Data){
+        let photo = Photo(imageData:imageData, title:title)
+        photos.append(photo)
+    }
     
+    func updatePhoto(photo: Photo, title: String, imageData: Data){
+        guard let index = photos.index(of: photo) else {return}
+        var scratch = photo
+        scratch.imageData = imageData
+        scratch.title = title
+        photos.remove(at: index)
+        photos.insert(scratch, at: index)
+    }
+    
+    var photos = [Photo]()
 }
