5d41e582d1313a8f08dffda6deb96039fb2f8f8c
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-13 20:59:46 -0500

    ThemeSelectionVC completed

--- Photo-Collection/Photo-Collection/View Controller/ThemeSelectionViewController.swift
+++ Photo-Collection/Photo-Collection/View Controller/ThemeSelectionViewController.swift
@@ -14,31 +14,15 @@ class ThemeSelectionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func selectDarkTheme(_ sender: Any) {
-    
+        themeHelper?.setThemePreferenceToDark()
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func selectAquaTheme(_ sender: Any) {
-    
+        themeHelper?.setThemePreferenceToAqua()
+        dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
