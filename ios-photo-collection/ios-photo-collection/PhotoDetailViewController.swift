8fb7058ead5e9458f9a653e3b5605fd5513a22f9
Ben Hakes <bhhakes@gmail.com>
2018-11-28 17:32:52 -0600

    setup PhotoDetailVC Scene & .sw

---
+++ ios-photo-collection/ios-photo-collection/PhotoDetailViewController.swift
@@ -0,0 +1,34 @@
+				//
+//  PhotoDetailViewController.swift
+//  ios-photo-collection
+//
+//  Created by Benjamin Hakes on 11/28/18.
+//  Copyright © 2018 Benjamin Hakes. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    @IBAction func addPhoto(_ sender: Any) {
+        
+    }
+    @IBAction func savePhoto(_ sender: Any) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
