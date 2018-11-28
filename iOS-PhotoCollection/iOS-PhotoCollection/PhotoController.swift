e6528ceacbb62c30181cb78763a7ec4c206b997b
jkaunert <jkaunert@clusterfly.net>
2018-11-28 14:24:42 -0600

    sets up model layer, adds Photo struct and PhotoController class

---
+++ iOS-PhotoCollection/iOS-PhotoCollection/PhotoController.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Foundation
+
+class PhotoController{
+    var photos: [Photo] = []
+    
+    func create(imageData: Data, title: String){
+        let photo = Photo(imageData: imageData,title: title)
+        photos.append(photo)
+    }
+    
+    func update(photo: Photo, data: Data, title: String){
+        
+    }
+}
