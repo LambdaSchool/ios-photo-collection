b1db371b7d3bf0dfce1d0b6e7ac6f3ae09cd6003
Ben Hakes <bhhakes@gmail.com>
2018-11-28 17:03:02 -0600

    add Theme helper - w/ some bugs to fix

--- ios-photo-collection/ios-photo-collection/ThemeHelper.swift
+++ ios-photo-collection/ios-photo-collection/ThemeHelper.swift
@@ -9,6 +9,33 @@
 import Foundation
 import UIKit
 
-class ThemeHelper {
+class ThemeHelper: UserDefaults {
+    
+    let themePreferenceKey: String
+    var themePreference: String?
+    
+    func setThemePerferenceToDark() {
+        UserDefaults.standard.set("Dark", forKey: themePreferenceKey)
+    }
+    
+    
+    // TODO: Test this color to make sure it works
+    func setThemePreferenceToBlue(){
+        UserDefaults.standard.set("Blue", forKey: themePreferenceKey)
+    }
+    
+    var themePerference: String? {
+        get {
+            return UserDefaults.standard.string(forKey: themePreferenceKey)
+        }
+    }
+    
+    init (){
+        
+        if themePreference == nil {
+            
+            self.setThemePerferenceToDark()
+        }
+    }
     
 }
