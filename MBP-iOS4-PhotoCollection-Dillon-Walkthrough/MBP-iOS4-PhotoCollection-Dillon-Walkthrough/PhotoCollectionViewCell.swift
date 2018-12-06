6832b7e5dd1dd7de117472d397ff54d091f89638
Vijay Das <vijaytdas@gmail.com>
2018-12-06 15:08:40 -0600

    laid out views and custom cases

---
+++ MBP-iOS4-PhotoCollection-Dillon-Walkthrough/MBP-iOS4-PhotoCollection-Dillon-Walkthrough/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  MBP-iOS4-PhotoCollection-Dillon-Walkthrough
+//
+//  Created by Vijay Das on 12/6/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
