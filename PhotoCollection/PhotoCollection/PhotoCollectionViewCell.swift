89af54ba169bdf7c6241b11d140fa18c158955ca
Sameera Leola <sameeraleola@gmail.com>
2018-12-06 15:27:08 -0600

    Set outlets on storyboard

--- PhotoCollection/PhotoCollection/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/PhotoCollectionViewCell.swift
@@ -9,5 +9,9 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = "PhotoCell"
     
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var photoTitleLabel: UILabel!
 }
