cd79554cc324f03aa649f0d61db2c7f8ecab08c0
Sameera Leola <sameeraleola@gmail.com>
2018-12-06 15:07:56 -0600

    Layed out views and set custom classes

---
+++ PhotoCollection/PhotoCollection/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Sameera Leola on 12/6/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ PhotoCollection/PhotoCollection/PhotoCollectionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoViewController.swift
+//  PhotoCollection
+//
+//  Created by Sameera Leola on 12/6/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewController: UICollection
+ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
