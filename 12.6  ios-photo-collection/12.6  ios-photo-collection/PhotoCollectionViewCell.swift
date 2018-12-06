a3622c59d4a9888e3f0c761aeeed160ee7ab3743
Sean Doyle <sdoylelambda@gmail.com>
2018-12-06 16:25:55 -0500

    Hook up views to view controllers.

--- 12.6  ios-photo-collection/12.6  ios-photo-collection/PhotoCollectionViewCell.swift
+++ 12.6  ios-photo-collection/12.6  ios-photo-collection/PhotoCollectionViewCell.swift
@@ -2,4 +2,10 @@ import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
     
+    let reuseIdentifier = "PhotoCell"
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    
 }
