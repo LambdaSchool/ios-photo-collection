514139201bd923bd5a0fada9cfb53ea5017b4c0e
Vijay Das <vijaytdas@gmail.com>
2018-12-06 15:56:55 -0600

    set up model and photo class

---
+++ MBP-iOS4-PhotoCollection-Dillon-Walkthrough/MBP-iOS4-PhotoCollection-Dillon-Walkthrough/PhotoModel.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoModel.swift
+//  MBP-iOS4-PhotoCollection-Dillon-Walkthrough
+//
+//  Created by Vijay Das on 12/6/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import Foundation
+
+class PhotoModel {
+    static let shared = PhotoModel()
+    private init () {}
+    // MARK: - Properties
+    private var photos: [Photo] = []
+    // MARK - CRUD Methods
+    // CRUD or MODEL METHODS
+    
+    func addPhoto(title: String, imageData: Data) {
+        let photo = Photo(title: title, imageData: imageData)
+        photos.append(photo)
+    }
+    
+    func remove(at indexPath: IndexPath) {
+        photos.remove(at: indexPath.row)
+    }
+    
+    func updatePhoto(photo: Photo, title: String, imageData: Data) {
+        photo.title = title
+        photo.imageData = imageData
+    }
+    
+    func photo(at indexPath: IndexPath) -> Photo {
+        return photos[indexPath.row]
+    }
+}
