6d17352ae5d11cf2498767466702145e9b80a7eb
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-16 10:32:16 -0500

    Add collection view scene

---
+++ PhotoCollectionApp/PhotoCollectionApp/PhotosCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollectionApp
+//
+//  Created by Nelson Gonzalez on 1/16/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
