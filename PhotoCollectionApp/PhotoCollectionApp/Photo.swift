33555d812ec1afd35b76367d33bb2c4e75934d2d
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-16 09:41:22 -0500

    Add Photo Struct

---
+++ PhotoCollectionApp/PhotoCollectionApp/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  PhotoCollectionApp
+//
+//  Created by Nelson Gonzalez on 1/16/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import Foundation
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
