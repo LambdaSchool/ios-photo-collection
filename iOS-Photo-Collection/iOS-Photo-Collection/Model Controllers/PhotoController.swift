4020c8ad4355b4a3ec8663e371638122cebb121b
Vijay Das <vijaytdas@gmail.com>
2019-01-16 16:22:12 -0600

    add Create and Update functions to PhotoController

--- iOS-Photo-Collection/iOS-Photo-Collection/Model Controllers/PhotoController.swift
+++ iOS-Photo-Collection/iOS-Photo-Collection/Model Controllers/PhotoController.swift
@@ -13,11 +13,17 @@ class PhotoController {
     
     var photos: [Photo] = []
     
-    func createPhoto() {
+    func createPhoto(imageData: Data, title: String) {
+        let photo = Photo(imageData: imageData, title: title)
+        photos.append(photo)
         
     }
     
-    func updatePhoto() {
+    func updatePhoto(photo: Photo, imageData: Data, title: String) {
+        var photo = photo
+        photo.imageData = imageData
+        photo.title = title
+        
         
     }
 }
