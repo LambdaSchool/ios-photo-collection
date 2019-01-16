168c2f13e3a5df92b200f19e6ea9098aafbcb5f4
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-16 10:55:26 -0500

    Add themeSelection scene with outlets/actions and auto layout

---
+++ PhotoCollectionApp/PhotoCollectionApp/ThemeSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollectionApp
+//
+//  Created by Nelson Gonzalez on 1/16/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    
+    
+    
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectDarkThemeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func selectLightThemeButton(_ sender: UIButton) {
+    }
+    
+}
