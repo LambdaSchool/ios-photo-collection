5748534aca76a89f95fb318431bcbfed88c194ea
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-16 12:57:40 -0800

    Set up initial view and created custom cell.

---
+++ PhotoCollection/PhotoCollection/Views/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Nathanael Youngren on 1/16/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+}
