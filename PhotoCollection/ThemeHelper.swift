89e856cba6eb43474163194c70ec90ac652ef8db
Stuart Sippel <stuartsippel@icloud.com>
2019-01-16 15:35:57 -0600

    Build model and model controller

---
+++ PhotoCollection/ThemeHelper.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by Stuart on 1/16/19.
+//  Copyright © 2019 Stuart. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    var themePreferenceKey: String
+    
+    func setThemePreferenceToDark() {
+        
+    }
+}
