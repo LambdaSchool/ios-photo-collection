b3333b4a79130733bbafc1d555820695483e8ef7
Moses Robinson <mosesjrobinson@icloud.com>
2019-01-16 14:20:56 -0800

    setting up photo detail and theme controllers

---
+++ Photo Collection/Photo Collection/PhotoDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoDetailViewController.swift
+//  Photo Collection
+//
+//  Created by Moses Robinson on 1/16/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+   
+
+}
---
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Moses Robinson on 1/16/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoNameLabel: UILabel!
+}
