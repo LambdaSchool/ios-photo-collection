4a00c400f60d2b136f399747252cb5ef29278ad0
Moses Robinson <mosesjrobinson@icloud.com>
2019-01-16 15:19:24 -0800

    implementing photo collection cell

--- Photo Collection/Photo Collection/PhotoDetailViewController.swift
+++ Photo Collection/Photo Collection/PhotoDetailViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class PhotoDetailViewController: UIViewController {
-
-   
-
+    
+    @IBAction func addPhoto(_ sender: Any) {
+        
+        
+    }
+    @IBAction func savePhoto(_ sender: Any) {
+        
+        
+    }
+    
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var photoNameText: UITextField!
+    
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
 }
---
+++ Photo Collection/Photo Collection/ThemeSelectionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeSelectionViewController.swift
+//  Photo Collection
+//
+//  Created by Moses Robinson on 1/16/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    @IBAction func selectDarkTheme(_ sender: Any) {
+        
+    }
+    @IBAction func selectPurpleTheme(_ sender: Any) {
+        
+    }
+    
+    
+    //MARK: - Properties
+    
+    var themeHelper: ThemeHelper? 
+
+}
