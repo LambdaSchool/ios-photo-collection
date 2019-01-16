1162e4e65e82cedf193a496fef5e3c6d6cf16a74
Vijay Das <vijaytdas@gmail.com>
2019-01-16 16:38:57 -0600

    add ThemeHelper

---
+++ iOS-Photo-Collection/iOS-Photo-Collection/Helpers/ThemeHelper.swift
@@ -0,0 +1,38 @@
+//
+//  ThemeHelper.swift
+//  iOS-Photo-Collection
+//
+//  Created by Vijay Das on 1/16/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ThemeHelper {
+    
+    let themePreferenceKey = "ThemePreferenceKey"
+    
+    func setThemePreferenceToDark() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Dark", forKey: themePreferenceKey)
+     }
+    
+    func setThemePreferenceToLight() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Light", forKey: themePreferenceKey)
+    }
+    
+    var themePreference: String? {
+        let userDefaults = UserDefaults.standard
+        return userDefaults.string(forKey: themePreferenceKey)
+    }
+    
+    private init () {
+        let userDefaults = UserDefaults.standard
+        if userDefaults.string(forKey: themePreferenceKey) == nil {
+            userDefaults.set("Light", forKey: themePreferenceKey)
+        }
+    }
+    
+}
