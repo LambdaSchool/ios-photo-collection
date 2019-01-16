71b16a5e630a2ecd5381de1c05e072cfc8895191
Jocelyn Stuart <luluscomputer@jocelyns-macbook-air.local>
2019-01-16 15:58:02 -0500

    completed IB page elements and partner swift files

---
+++ PhotoCollection/PhotoCollection/PhotoDetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollection
+//
+//  Created by Jocelyn Stuart on 1/16/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    
+    @IBAction func addPhoto(_ sender: UIButton) {
+    }
+    
+    @IBAction func savePhoto(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PhotoCollection/PhotoCollection/ThemeSelectionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Jocelyn Stuart on 1/16/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    var themeHelper: ThemeHelper?
+    
+    @IBAction func selectDarkTheme(_ sender: UIButton) {
+    }
+    
+    @IBAction func selectPinkTheme(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
