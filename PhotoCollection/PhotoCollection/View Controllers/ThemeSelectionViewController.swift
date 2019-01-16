d743f15d2e5b06cabf5c366e725ccd1186f16801
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-16 13:29:56 -0800

    Setup theme selection view and vc.

---
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Nathanael Youngren on 1/16/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    @IBAction func selectDarkTheme(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func selectYellowTheme(_ sender: UIButton) {
+        
+    }
+    
+    var themeHelper: ThemeHelper?
+
+}
