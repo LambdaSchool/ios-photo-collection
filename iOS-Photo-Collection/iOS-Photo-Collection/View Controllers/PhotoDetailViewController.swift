edd5f4c0c2aa6517a925d4dc76f6ed6239a01890
Vijay Das <vijaytdas@gmail.com>
2019-01-16 17:03:26 -0600

    add CollectionViewController scene

---
+++ iOS-Photo-Collection/iOS-Photo-Collection/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoDetailViewController.swift
+//  iOS-Photo-Collection
+//
+//  Created by Vijay Das on 1/16/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    
+    var photoController = PhotoController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS-Photo-Collection/iOS-Photo-Collection/Views/PhotosCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotosCollectionViewCell.swift
+//  iOS-Photo-Collection
+//
+//  Created by Vijay Das on 1/16/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var title: UILabel!
+    
+    
+}
