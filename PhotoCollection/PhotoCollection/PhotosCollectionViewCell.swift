3b11b22fa8853fc352ea8269ca5e618838d9114a
Jocelyn Stuart <luluscomputer@jocelyns-macbook-air.local>
2019-01-16 15:38:19 -0500

    add photo collection page to IB

---
+++ PhotoCollection/PhotoCollection/PhotosCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Jocelyn Stuart on 1/16/19.
+//  Copyright © 2019 JS. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var photoLabel: UILabel!
+    
+    
+}
