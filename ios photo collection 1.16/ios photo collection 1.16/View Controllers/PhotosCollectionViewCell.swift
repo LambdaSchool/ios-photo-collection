4c3cb82ac4d39bbe8bbb097b3855bb647a9e1b23
Sean Doyle <sdoylelambda@gmail.com>
2019-01-16 17:11:26 -0500

    Theme view and controller with outlets.

--- ios photo collection 1.16/ios photo collection 1.16/View Controllers/PhotosCollectionViewCell.swift
+++ ios photo collection 1.16/ios photo collection 1.16/View Controllers/PhotosCollectionViewCell.swift
@@ -2,6 +2,16 @@ import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
     
+    var photo: Photo? {
+        didSet {
+            updateView()
+        }
+        }
+    
+    func updateView() {
+        
+    }
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var labelOutlet: UILabel!
 }
---
+++ ios photo collection 1.16/ios photo collection 1.16/View Controllers/ThemeHelperViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+class ThemeHelperViewController: UIViewController {
+    
+    var themeHelper: ThemeHelper?
+    
+    @IBAction func selectDarkThem(_ sender: Any) {
+    }
+    
+    @IBAction func selectLightTheme(_ sender: Any) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
