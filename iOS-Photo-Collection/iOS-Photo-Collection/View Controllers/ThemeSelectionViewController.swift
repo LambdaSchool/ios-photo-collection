16dcc987cbfa7b05eaebac4da03eb1675c49b6ff
Vijay Das <vijaytdas@gmail.com>
2019-01-16 17:43:53 -0600

    add Theme Selection scene and ThemeSelectionViewController

---
+++ iOS-Photo-Collection/iOS-Photo-Collection/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ThemeSelectionViewController.swift
+//  iOS-Photo-Collection
+//
+//  Created by Vijay Das on 1/16/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    
+    var themeHelper: ThemeHelper?
+    
+    @IBAction func setDarkTheme(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+    @IBAction func setLightTheme(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
