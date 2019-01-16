3d1bda034ebe02669a11f546917de5e4b9b1906d
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-16 09:46:28 -0500

    Add PhotoController with methods

---
+++ PhotoCollectionApp/PhotoCollectionApp/PhotoController.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoController.swift
+//  PhotoCollectionApp
+//
+//  Created by Nelson Gonzalez on 1/16/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import Foundation
+class PhotoController {
+    var photos: [Photo] = []
+    
+    
+    func createPhotos(with data: Data, and title: String) {
+        let photo = Photo(imageData: data, title: title)
+        photos.append(photo)
+    }
+    
+    func update(photo: Photo, data: Data, string: String){
+        
+    }
+}
