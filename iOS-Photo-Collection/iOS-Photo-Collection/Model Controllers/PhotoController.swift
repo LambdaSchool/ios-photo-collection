d9b41258d12ce8dcde98d7b3bb8a1f3bb66dd613
Vijay Das <vijaytdas@gmail.com>
2019-01-16 14:33:49 -0600

    add Photo and PhotoModel

---
+++ iOS-Photo-Collection/iOS-Photo-Collection/Model Controllers/PhotoController.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoController.swift
+//  iOS-Photo-Collection
+//
+//  Created by Vijay Das on 1/16/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoController {
+    
+    var photos: [Photo] = []
+    
+    func createPhoto() {
+        
+    }
+    
+    func updatePhoto() {
+        
+    }
+}
