69e5dea2a9bd3e8db7c69732c943bfc02b8f3438
Stuart Sippel <stuartsippel@icloud.com>
2019-01-23 12:58:56 -0600

    Setup storyboard

---
+++ Photo Collection/Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection
+//
+//  Created by Stuart on 1/23/19.
+//  Copyright © 2019 Stuart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
