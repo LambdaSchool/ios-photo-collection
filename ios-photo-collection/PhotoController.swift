6bb185817f188b0bcdeff6eb0490d5019e01c300
Taylor Lyles <hawks93187@gmail.com>
2019-05-02 13:23:50 -0700

    Model Layer Set Up

---
+++ ios-photo-collection/PhotoController.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoController.swift
+//  ios-photo-collection
+//
+//  Created by Taylor Lyles on 5/2/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import Foundation
+
+class PhotoController {
+    
+    var photos: [Photo] = []
+    
+    init() {
+        
+    }
+    
+    func create() {
+        
+        photos.append =
+        
+    }
+    
+    func upadte() {
+        
+        
+        
+    }
+    
+}
---
+++ ios-photo-collection/ThemeHelper.swift
@@ -0,0 +1,32 @@
+//
+//  ThemeHelper.swift
+//  ios-photo-collection
+//
+//  Created by Taylor Lyles on 5/2/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class ThemeHelper {
+    
+    let themePreferenceKey: String = "Dark"
+    
+    var themePrefernce: String?
+    
+    func setThemePreferenceToDark() {
+        
+//        let userDefaults = UserDefaults.standard
+//        userDefaults.set()
+        
+        UserDefaults.standard.set(ForKey: )
+        
+    }
+    
+    func setThemePreferenceToYourColorHere() {
+        
+    }
+    
+   
+    
+}
