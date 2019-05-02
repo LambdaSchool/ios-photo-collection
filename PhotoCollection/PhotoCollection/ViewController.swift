86cc5e2ab6cfc79a5610553ef2efa33de37cb3fd
Kobe McKee <kobe.mckee@gmail.com>
2019-05-02 12:54:47 -0600

    Creation of new project

---
+++ PhotoCollection/PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Kobe McKee on 5/2/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
