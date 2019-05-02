a7c30284eaedf377326fc4d556bb181057ed2f94
Alex Perse <alexanderperse@gmail.com>
2019-05-02 19:53:50 -0400

    Added storyboard

---
+++ PhotoCollection/PhotoCollection/PhotosCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Alex Perse on 5/2/19.
+//  Copyright © 2019 Alex Perse. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    
+    
+}
---
+++ PhotoCollection/PhotoCollection/PhotosCollectionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PhotosCollectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Alex Perse on 5/2/19.
+//  Copyright © 2019 Alex Perse. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
