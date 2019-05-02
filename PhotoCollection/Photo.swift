2204a487b798edde811af82b9e74c8009701287b
Alex Perse <alexanderperse@gmail.com>
2019-05-02 15:42:26 -0400

    Added Photo.swift

---
+++ PhotoCollection/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Alex Perse on 5/2/19.
+//  Copyright © 2019 Alex Perse. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    
+    var imageData: Data
+    var title: String
+    
+}
