e59e1b811a5cdabbd252f91c0b78e941ec7d13ea
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-02 14:24:34 -0600

    Created a collection VC and added UI elements to a collection view cell

---
+++ PhotoCollection/PhotoCollection/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Mitchell Budge on 5/2/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+    
+}
