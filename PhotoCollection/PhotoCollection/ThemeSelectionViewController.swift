0e42b39f436cf10e4cd1b1bba8af5c4ed43d9b7d
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-02 14:49:35 -0600

    Created a theme selection VC with UI elements to select between dark and light themes.

---
+++ PhotoCollection/PhotoCollection/ThemeSelectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Mitchell Budge on 5/2/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    //var themeHelper: ThemeHelper?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    @IBAction func selectLightTheme(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
