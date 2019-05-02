fc64565c844457a3a803beafbd11d92cb56c3c41
Christopher Aronson <christopheraronson@icloud.com>
2019-05-02 14:09:39 -0700

    Added PhtoCollectionViewCell class/file and implemented updateviews

---
+++ PhotoCollection/PhotoCollection/PhotoCollectionViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Christopher Aronson on 5/2/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+
+    
+    func updateViews() {
+        guard let photo = photo else {return}
+        
+        imageView.image = UIImage(data: photo.imageData)
+        imageLabel.text = photo.title
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageLabel: UILabel!
+}
