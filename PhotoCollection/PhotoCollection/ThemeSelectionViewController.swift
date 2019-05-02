e00cfb963a09276b392f88929d849dfa183bfe5e
jayfenam <jonathanferrer95@gmail.com>
2019-05-02 16:31:53 -0400

    Add selectTheme storyboard and segue

---
+++ PhotoCollection/PhotoCollection/ThemeSelectionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Jonathan Ferrer on 5/2/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    @IBAction func selectBlueTheme(_ sender: Any) {
+    }
+    
+}
