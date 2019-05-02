346c7cee234ac7bccba866956679bb5972cbacd8
Dan Morse <morsedan@outlook.com>
2019-05-02 14:14:33 -0700

    added CollectionViewController and it's class.

---
+++ PhotoCollection/PhotoCollection/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by morse on 5/2/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    
+}
