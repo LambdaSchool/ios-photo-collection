bab2e3d0f7613cb855b2b618d791ee2eddbc7a7d
Christopher Aronson <christopheraronson@icloud.com>
2019-05-02 12:31:46 -0700

    Created Photo class/file. Add print name statement

---
+++ PhotoCollection/PhotoCollection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Christopher Aronson on 5/2/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
