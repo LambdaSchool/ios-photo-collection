0d214ca4467421a4bbac8ed8bb2b03928ef5d973
Christopher Aronson <christopheraronson@icloud.com>
2019-05-02 12:25:57 -0700

    init Commit

---
+++ PhotoCollection/PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Christopher Aronson on 5/2/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
