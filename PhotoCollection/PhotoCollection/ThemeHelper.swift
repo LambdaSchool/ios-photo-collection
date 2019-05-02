a55c693636d64126b4a1c3292c2463da89093dd2
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-02 16:40:56 -0400

    Added ThemeHelper

---
+++ PhotoCollection/PhotoCollection/ThemeHelper.swift
@@ -0,0 +1,33 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by Thomas Cacciatore on 5/2/19.
+//  Copyright © 2019 Thomas Cacciatore. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    let themePreferenceKey: String
+    var themePreference: String? {
+        
+        return UserDefaults.standard.string(forKey: themePreferenceKey)
+       
+    }
+    
+    init(themePreferenceKey: String, themePreference: String?) {
+        self.themePreferenceKey = themePreferenceKey
+        if themePreference == nil {
+            setThemePreferenceToPurple()
+        }
+    }
+    
+    func setThemePreferenceToDark() {
+        UserDefaults.standard.set("Dark", forKey: themePreferenceKey)
+    }
+    
+    func setThemePreferenceToPurple() {
+        UserDefaults.standard.set("Purple", forKey: themePreferenceKey)
+    }
+}
