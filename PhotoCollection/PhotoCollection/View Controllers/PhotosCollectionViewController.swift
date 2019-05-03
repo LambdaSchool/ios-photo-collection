e4718cacd8f76ace42229d8c6f845b8d44ba04b8
alexladines <alexladines@outlook.com>
2019-05-02 20:10:14 -0700

    Finished PhotoCollectionViewController and ThemeSelectionViewController.

--- PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewController.swift
@@ -10,23 +10,72 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    let photoController = PhotoController()
+    let themeHelper = ThemeHelper()
+    
+    // MARK: - PhotosCollectionViewController
+    func setTheme() {
+        // Get curent theme
+        guard let currentThemeHelper = self.themeHelper.themePreference else { return }
+        
+        self.collectionView.backgroundColor = (currentThemeHelper == String.dark) ? UIColor.darkGray: UIColor.blue
     }
     
-    
-    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
-    /*
+    // MARK: - UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photoController.photos.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotosCollectionViewCell
+        
+        // The didset method handles the UI updates.
+        cell.photo = self.photoController.photos[indexPath.item]
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "SelectTheme"
+        {
+            guard let themeSelectionVC = segue.destination as? ThemeSelectionViewController else { return }
+            
+            themeSelectionVC.themeHelper = themeHelper
+        }
+        else if segue.identifier == "AddPhoto"
+        {
+            guard let photoDetailVC = segue.destination as? PhotoDetailViewController else { return }
+            
+            photoDetailVC.photoController = self.photoController
+            photoDetailVC.themeHelper = self.themeHelper
+            
+        }
+        else if segue.identifier == "ShowPhoto"
+        {
+            guard let indexPaths = self.collectionView.indexPathsForSelectedItems,
+                let photoDetailVC = segue.destination as? PhotoDetailViewController else { return }
+            
+            // Selected item
+            let indexPath = indexPaths[0]
+            
+            // Values to be passed
+            photoDetailVC.photo = self.photoController.photos[indexPath.item]
+            photoDetailVC.photoController = self.photoController
+            photoDetailVC.themeHelper = self.themeHelper
+        }
     }
-    */
+    
 
 }
+
--- PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -10,31 +10,29 @@ import UIKit
 
 class ThemeSelectionViewController: UIViewController {
 
+    // MARK: - Properties
     var themeHelper: ThemeHelper?
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK: - IBAction
     @IBAction func selectDarkTheme(_ sender: Any) {
-        
+        guard let themeHelper = self.themeHelper else { return }
+        themeHelper.setThemePreferenceToDark()
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func selectBlueTheme(_ sender: Any) {
-        
+        guard let themeHelper = self.themeHelper else { return }
+        themeHelper.setThemePreferenceToBlue()
+        dismiss(animated: true, completion: nil)
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
