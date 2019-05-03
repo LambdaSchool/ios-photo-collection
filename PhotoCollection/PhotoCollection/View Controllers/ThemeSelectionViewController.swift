73c5cc9ec57c0c96a130124c72d230f61208cc7c
alexladines <alexladines@outlook.com>
2019-05-02 18:04:41 -0700

    ThemeSelectionViewController Scene finished. Need to check constrains on collection view cell.

---
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Alex Ladines on 5/2/19.
+//  Copyright © 2019 Alex Perse. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectDarkTheme(_ sender: Any) {
+        
+    }
+    
+    @IBAction func selectBlueTheme(_ sender: Any) {
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
