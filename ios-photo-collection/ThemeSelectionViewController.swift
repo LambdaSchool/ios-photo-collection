2f61bfdeab870c169545afd8236d40272be3e8a7
Taylor Lyles <hawks93187@gmail.com>
2019-05-02 17:09:50 -0700

    themeSelectionViewController

--- ios-photo-collection/ThemeSelectionViewController.swift
+++ ios-photo-collection/ThemeSelectionViewController.swift
@@ -20,7 +20,33 @@ class ThemeSelectionViewController: UIViewController {
     
 
 	@IBAction func buttonSelectorValueChanged(_ sender: UISegmentedControl) {
+		// Get selected index of the button
+		// Dark is at index 0, Neon pink is at index 1
+		// If the index is 0 we set the theme to dark, if index is 0 we set theme to neon pink
+		// Dismiss current controller
+		
+		switch sender.selectedSegmentIndex {
+		case 0:
+			themeHelper?.setThemePreferenceToDark()
+		case 1:
+			themeHelper?.setThemePreferenceToNeonPink()
+		default:
+			break
+		}
+		dismiss(animated: true)
 	}
 	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		if let currentTheme = themeHelper?.themePreference {
+			if currentTheme == "Dark" {
+				themeSelector.selectedSegmentIndex = 0
+			} else {
+				themeSelector.selectedSegmentIndex = 1
+			}
+		}
+		
+	}
 
+	@IBOutlet weak var themeSelector: UISegmentedControl!
 }
