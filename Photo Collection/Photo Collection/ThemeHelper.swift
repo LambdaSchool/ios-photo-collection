0afdceb2d09e12ec196ecb3eb337f8176e3abe2b
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-09 20:17:02 -0600

    ThemeHelper.swift
    Reconfigured original theme helper and fixed the .themePreferenceKey issue.

--- Photo Collection/Photo Collection/ThemeHelper.swift
+++ Photo Collection/Photo Collection/ThemeHelper.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+extension String {
+    static var themePreferenceKey = "themePreferenceKey"
+}
+
+class ThemeHelper {
+    init() {
+        if themePreference == "" {
+            setThemePreferenceToDark()
+        }
+    }
+    
+    var themePreference: String {
+        let userDefaults = UserDefaults.standard
+        guard let theme = userDefaults.string(forKey: .themePreferenceKey) else { return "" }
+        return theme
+    }
+    
+    func setThemePreferenceToDark() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Dark", forKey: .themePreferenceKey)
+    } // end of set theme to dark function
+    
+    
+    func setThemePreferenceToGray() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Gray", forKey: .themePreferenceKey)
+    } // end of set theme to gray function
+}
