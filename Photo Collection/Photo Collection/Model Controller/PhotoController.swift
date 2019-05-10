8020b63fecfcdb776e2df078dbe8abe63914c570
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-09 20:15:02 -0600

    PhotoController.swift
    Set up the model controller file.

--- Photo Collection/Photo Collection/Model Controller/PhotoController.swift
+++ Photo Collection/Photo Collection/Model Controller/PhotoController.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class PhotoController {
+    
+    var photos: [Photo] = []
+    
+    func createPhoto(imageData: Data, title: String) {
+        let newPhoto = Photo(imageData: imageData, title: title)
+        photos.append(newPhoto)
+    } // end of create photo function
+    
+    func updatePhoto(photo: Photo, imageData: Data, title: String) {
+        guard let photoIndex = photos.firstIndex(of: photo) else { return }
+        var updatedPhoto = photo
+        photos.remove(at: photoIndex)
+        
+        updatedPhoto.imageData = imageData
+        updatedPhoto.title = title
+        photos.append(updatedPhoto)
+    } // end of update photo function
+}
