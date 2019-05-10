54be3f9996ef93f2779a92d90a0f036bcdd7dc86
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-09 20:33:21 -0600

    UI Interface built
    Set up the three main views (one collection and two regular)
    Added labels, buttons, and image views that change the display and functionality of the photo collection.

---
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Mitchell Budge on 5/9/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
