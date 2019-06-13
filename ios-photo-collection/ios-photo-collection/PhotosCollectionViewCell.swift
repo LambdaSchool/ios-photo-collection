ad143a4d4d49541c011025278a4e779bc145945d
Ife Banire <IfeBanire@yahoo.com>
2019-06-13 17:59:36 -0400

    Part 3 completed, Storyboard setup

---
+++ ios-photo-collection/ios-photo-collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCollectionViewCell.swift
+//  ios-photo-collection
+//
+//  Created by Lambda_School_Loaner_101 on 6/13/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photosLabel: UILabel!
+    @IBOutlet weak var photosImageView: UIImageView!
+    
+    
+}
---
+++ ios-photo-collection/ios-photo-collection/ThemeSelectionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThemeSelectionViewController.swift
+//  ios-photo-collection
+//
+//  Created by Lambda_School_Loaner_101 on 6/13/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    
+    @IBAction func selectBlueTheme(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
