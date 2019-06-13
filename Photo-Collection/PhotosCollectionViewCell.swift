6d88d99691ecaeb3da511801b18e903e82b0a12c
Kat Milton <mltnkat@gmail.com>
2019-06-13 15:02:56 -0700

    Updated UI.

---
+++ Photo-Collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo-Collection
+//
+//  Created by Kat Milton on 6/13/19.
+//  Copyright © 2019 Kat Milton. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    
+}
