6ef6fc586c9c2d27c056bc9863b300d3fd13bd37
Jake Connerly <jlconnerly@gmail.com>
2019-07-25 14:56:19 -0500

    Completed Part2

---
+++ iOS Photo Collection/iOS Photo Collection/Controllera/ThemeHelper.swift
@@ -0,0 +1,48 @@
+//
+//  ThemeHelper.swift
+//  iOS Photo Collection
+//
+//  Created by Jake Connerly on 7/25/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    
+    //
+    //MARK: - Properties
+    //
+    
+    static let themePreferenceKey: String = ""
+    
+    var themePreference: String? {
+        let userDefaults = UserDefaults.standard
+        let preference = userDefaults.string(forKey: ThemeHelper.themePreferenceKey)
+        return preference
+    }
+    
+    //
+    // MARK: - Methods
+    //
+    
+    func setThemePreferenceToDark() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Dark", forKey: ThemeHelper.themePreferenceKey)
+    }
+    
+    func setThemePreferenceToYourColorHere() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Blue", forKey: ThemeHelper.themePreferenceKey)
+    }
+    
+    #warning("Check later if correct")
+    
+    init(themePreference: String?) {
+        if themePreference == nil {
+            setThemePreferenceToDark()
+        }
+        
+    }
+    
+}
---
+++ iOS Photo Collection/iOS Photo Collection/Models/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  iOS Photo Collection
+//
+//  Created by Jake Connerly on 7/25/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var ImageData: Data
+    var title: String
+}
