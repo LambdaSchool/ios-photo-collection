a484856418bf5b7fd80a00e4605707726e7d510f
Steven <Sleyva1991@outlook.com>
2019-07-25 16:30:06 -0400

    Started on storyboard

---
+++ iOS Photo Collection/iOS Photo Collection/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoDetailViewController.swift
+//  iOS Photo Collection
+//
+//  Created by Steven Leyva on 7/25/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS Photo Collection/iOS Photo Collection/View/PhotosCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosCollectionViewCell.swift
+//  iOS Photo Collection
+//
+//  Created by Steven Leyva on 7/25/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+    
+}
