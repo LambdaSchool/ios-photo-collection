187dbf3e8327acb74e580dda2a466f4c1b00f17a
Steven <Sleyva1991@outlook.com>
2019-07-25 18:36:40 -0400

    added more code

--- iOS Photo Collection/iOS Photo Collection/View Controllers/PhotoDetailViewController.swift
+++ iOS Photo Collection/iOS Photo Collection/View Controllers/PhotoDetailViewController.swift
@@ -27,9 +27,27 @@ class PhotoDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
+       
     }
+    func setTheme() {
+        guard let theme = themeHelper?.themePreference else { return }
+        
+        if theme == "Dark" {
+            view.backgroundColor = .darkGray
+        } else if theme == "Blue" {
+            view.backgroundColor = .blue
+        }
+    }
+    
+    func updateViews() {
+        setTheme()
+        guard let photo = photo else { return }
+        
+        imageView.image = UIImage(data: photo.ImageData)
+        textField.text = photo.title
+    }
+    
     
     //
     //MARK: - IBActions and Methods
@@ -38,6 +56,16 @@ class PhotoDetailViewController: UIViewController {
     @IBAction func addPhoto(_ sender: UIButton) {
     }
     @IBAction func sveButtonTapped(_ sender: UIBarButtonItem) {
+        
+        guard let photoController = photoController, let title = textField.text, let newPhoto = imageView.image?.pngData() else { return }
+        if photo == nil {
+           photoController.createPhoto(image: newPhoto, title: title)
+        } else {
+            guard let photo = photo else { return }
+            photoController.update(photo: photo, image: newPhoto, title: photo.title)
+            
+        }
+        navigationController?.popViewController(animated: true)
     }
     
     /*
