ad9b4cbeaa1fc7157281687837aeaf5d6df5fef1
Andrew Ruiz <nukedoom@gmail.com>
2019-07-25 13:07:52 -0700

    Created photo structure

---
+++ Photo Collection  PhotoCollection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection  PhotoCollection
+//
+//  Created by Andrew Ruiz on 7/25/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
---
+++ Photo Collection  PhotoCollection/PhotoController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoController.swift
+//  Photo Collection  PhotoCollection
+//
+//  Created by Andrew Ruiz on 7/25/19.
+//  Copyright © 2019 Andrew Ruiz. All rights reserved.
+//
+
+import Foundation
