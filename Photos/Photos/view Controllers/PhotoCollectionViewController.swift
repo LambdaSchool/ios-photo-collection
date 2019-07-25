bfcf3f86c69e6f580128bd552845f68283a4e80e
bfvilchez <bfvilchez@icloud.com>
2019-07-25 13:39:30 -0600

    set up story board scenes

---
+++ Photos/Photos/view Controllers/PhotoCollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by brian vilchez on 7/25/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
