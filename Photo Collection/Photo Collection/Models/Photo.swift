ca3f9d6be0c7e24b10033fad371b49962a11e2af
dillonp23 <dillonp.lambda@gmail.com>
2019-08-15 19:03:35 -0700

    Photo struct, Theme controller, and PhotoController file added

---
+++ Photo Collection/Photo Collection/Models/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Dillon P on 8/15/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var title: String
+    var imageData: Data
+}
