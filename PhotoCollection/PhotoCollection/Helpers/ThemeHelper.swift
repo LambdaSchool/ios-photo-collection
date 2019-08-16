ad7e36b2bb9c0ffb37f6f2156bc705c9771b5128
John Kouris <jdkouris@gmail.com>
2019-08-15 18:07:53 -0700

    Added the Helpers group Added the ThemeHelper class

---
+++ PhotoCollection/PhotoCollection/Helpers/ThemeHelper.swift
@@ -0,0 +1,37 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by John Kouris on 8/15/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    let themePreferenceKey = "themePreferenceKey"
+    
+    var themePreference: String? {
+        let userDefaults = UserDefaults.standard
+        
+        return userDefaults.string(forKey: themePreferenceKey)
+    }
+    
+    func setThemePreferenceToDark() {
+        let userDefaults = UserDefaults.standard
+        
+        userDefaults.set("Dark", forKey: themePreferenceKey)
+    }
+    
+    func setThemePreferenceToYourColorHere() {
+        let userDefaults = UserDefaults.standard
+        
+        userDefaults.set("Blue", forKey: themePreferenceKey)
+    }
+    
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToDark()
+        }
+    }
+}
