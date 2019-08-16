17b1905dc643bfc048e3f80291f14549058b43fb
dillonp23 <dillonp.lambda@gmail.com>
2019-08-15 19:49:50 -0700

    Add SelectThemeViewController

---
+++ Photo Collection/Photo Collection/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThemeSelectionViewController.swift
+//  Photo Collection
+//
+//  Created by Dillon P on 8/15/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    var themeHelper: ThemeHelper?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    
+    @IBAction func selectTealTheme(_ sender: Any) {
+    }
+    
+
+}
