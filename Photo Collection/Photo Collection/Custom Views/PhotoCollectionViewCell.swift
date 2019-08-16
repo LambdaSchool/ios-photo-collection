650d8747a525d3fbcc06be3f738a92438ac6513a
dillonp23 <dillonp.lambda@gmail.com>
2019-08-15 19:22:27 -0700

    Basic app UI and outlets

---
+++ Photo Collection/Photo Collection/Custom Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Dillon P on 8/15/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    
+}
