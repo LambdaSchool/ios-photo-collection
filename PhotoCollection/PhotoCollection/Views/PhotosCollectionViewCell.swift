c4951669d192a3910cbf4d20d0f39504bb8f4f77
John Kouris <jdkouris@gmail.com>
2019-08-15 18:14:27 -0700

    Designed CollectionViewController UI with storyboard Added the PhotosCollectionViewController and PhotosCollectionViewCell files

---
+++ PhotoCollection/PhotoCollection/Views/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by John Kouris on 8/15/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
