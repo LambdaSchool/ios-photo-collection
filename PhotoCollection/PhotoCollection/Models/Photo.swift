e1dc0cd1960a93e65a748add4cc0b119ca574819
John Kouris <jdkouris@gmail.com>
2019-08-15 17:55:06 -0700

    Added groups for files Added the Photo model

---
+++ PhotoCollection/PhotoCollection/Models/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by John Kouris on 8/15/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
