29672ff22892bf1e5a846ffc5e513ad44eb6739e
dillonp23 <dillonp.lambda@gmail.com>
2019-08-15 18:25:32 -0700

    initial commit

---
+++ Photo Collection/Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection
+//
+//  Created by Dillon P on 8/15/19.
+//  Copyright © 2019 Dillon P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
