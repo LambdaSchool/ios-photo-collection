546f771c606007f2897a632c7ebc264ce71eb955
JosephRogers94 <Joerogersss2016@gmail.com>
2019-08-18 01:27:14 -0500

    Model Layer Set Up

--- Photo Collection/Photo Collection/Model Controller/PhotoController.swift
+++ Photo Collection/Photo Collection/Model Controller/PhotoController.swift
@@ -16,7 +16,13 @@ class PhotoController {
         photos.append(photoAdded)
     }
     
-    func updatePhoto(photo: Photo, newdata: Data, newTitle: String) {
+    func updatePhoto(photoToUpdate: Photo, newData: Data, newTitle: String) {
+       //this is creating an index path. An index path is the spot that is chosen. first index is grabbing the first index that the user tries to use, then changes the properties accordingly.
+        guard let index = photos.firstIndex(of: photoToUpdate) else {return}
+        
+        photos[index].title = newTitle
+        photos[index].imageData = newData
+        
         
     }
 }
