4e1a85e9d968debeaf2286704b3b1197fb682bc5
bfvilchez <bfvilchez@icloud.com>
2019-08-22 14:48:35 -0600

    set up navigation files

---
+++ PhotoCollection2/PhotoCollection2/views/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoCollection2
+//
+//  Created by brian vilchez on 8/22/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
