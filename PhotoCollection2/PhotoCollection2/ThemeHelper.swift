ee2410703e468b00c2c65736fceb83af4f03a05e
bfvilchez <bfvilchez@icloud.com>
2019-08-22 14:41:40 -0600

    handing off project

--- PhotoCollection2/PhotoCollection2/ThemeHelper.swift
+++ PhotoCollection2/PhotoCollection2/ThemeHelper.swift
@@ -9,13 +9,37 @@
 import Foundation
 
 extension String {
-    static let DarkThemeKey = "DarkTheme"
-    static let lightThemeKey = "LightTheme"
+    static let themePreferenceKey = "themePreferenceKey"
+    
 }
+
 
 class ThemeHelper {
     
-    var themePreference: String?
+    var themePreference: String? {
+        
+        let selectedTheme = UserDefaults.standard.string(forKey: .themePreferenceKey)
+        if selectedTheme == "Dark" {
+            setThemePreferenceToDark()
+        } else if selectedTheme == "blue" {
+            setThemePreferenceToAnotherColor()
+        }
+        return selectedTheme
+    }
     
-   
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToDark()
+        }
+    }
+    
+    func setThemePreferenceToDark() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("Dark", forKey: .themePreferenceKey)
+    }
+    
+    func setThemePreferenceToAnotherColor() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set("blue", forKey: .themePreferenceKey)
+    }
 }
---
+++ PhotoCollection2/PhotoCollection2/ViewControllers/PhotoDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollection2
+//
+//  Created by brian vilchez on 8/22/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    var photoController: PhotoController?
+    var photo:Photo?
+    var themeHelper: ThemeHelper?
+    
+    @IBOutlet weak var PhotoImage: UIImageView!
+    @IBOutlet weak var photoTextField: UITextField!
+    
+    @IBAction func addPhoto(_ sender: UIButton){
+        
+    }
+    @IBAction func savePhoto(_ sender: UIBarButtonItem){
+        
+    }
+
+}
