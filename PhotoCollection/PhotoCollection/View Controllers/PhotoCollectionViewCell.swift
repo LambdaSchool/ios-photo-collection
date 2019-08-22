7b4ee0b9e96e74bc19c8aefa23782e57878d8e6a
Donovan Langager <donovan.lan16@gmail.com>
2019-08-22 12:52:45 -0700

    Added files

---
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Uptiie on 8/22/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
