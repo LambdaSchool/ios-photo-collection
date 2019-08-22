de78c9966df67192bb31778da2136717f72dcdfa
bfvilchez <bfvilchez@icloud.com>
2019-08-22 13:59:37 -0600

    added a new project folder

---
+++ PhotoCollection2/PhotoCollection2/ThemeHelper.swift
@@ -0,0 +1,21 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection2
+//
+//  Created by brian vilchez on 8/22/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static let DarkThemeKey = "DarkTheme"
+    static let lightThemeKey = "LightTheme"
+}
+
+class ThemeHelper {
+    
+    var themePreference: String?
+    
+   
+}
---
+++ PhotoCollection2/PhotoCollection2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection2
+//
+//  Created by brian vilchez on 8/22/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
