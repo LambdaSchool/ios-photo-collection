abf0bac71f436e1faaf42a47d390cc0dba43827a
Wilma <kima_92@fastmail.com>
2019-09-19 18:01:32 -0400

    Storyboard configured.

---
+++ Photo Collection/Photo Collection/Views/PhotosCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by macbook on 9/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    
+    //MARK: - Outlets
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+}
