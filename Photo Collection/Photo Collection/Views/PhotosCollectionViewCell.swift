bbc9473468fcebf2e986d8713e7bad50d191eed0
Wilma <kima_92@fastmail.com>
2019-09-19 18:52:48 -0400

    Configured PhotoCollectionViewCell.

--- Photo Collection/Photo Collection/Views/PhotosCollectionViewCell.swift
+++ Photo Collection/Photo Collection/Views/PhotosCollectionViewCell.swift
@@ -11,8 +11,31 @@ import UIKit
 class PhotosCollectionViewCell: UICollectionViewCell {
     
     
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
+    
+    
+    
     //MARK: - Outlets
     @IBOutlet weak var textLabel: UILabel!
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    func updateViews() {
+        
+        guard let photo = photo else { return }
+        
+        let image = UIImage(data: photo.imageData)
+        textLabel.text = photo.title
+        imageView.image = image
+        
+    }
+    
+    
     
 }
