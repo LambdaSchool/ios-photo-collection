60495ab099eca70b53bddb6e101db959af59f678
Wilma <kima_92@fastmail.com>
2019-09-19 16:47:55 -0400

    ThemeHelper class updated to set Theme Preference

--- Photo Collection/Photo Collection/View Controllers/ThemeHelper.swift
+++ Photo Collection/Photo Collection/View Controllers/ThemeHelper.swift
@@ -11,12 +11,13 @@ import Foundation
 
 class ThemeHelper {
     
-    let themePreferenceKey: String = "themePreference"     // Not sure if it should be initialized as an optional or an empty string
+    let themePreferenceKey: String = "themePreference" 
     
     
     var themePreference: String? {
         return UserDefaults.standard.string(forKey: themePreferenceKey)
     }
+//    ^^ should return an optional string because if the user's theme preference hasn't been saved, the value returned from UserDefaults will be nil.
     
     
     
@@ -34,18 +35,13 @@ class ThemeHelper {
     
     
     
-    
-    
-    
-    
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToDark()
+        }
+    }
+
     
 }
 
 
-
-//  TODO: - Part 2, step 4 and 5   of intructions
-//
-//Finally, create a computed property called themePreference of type String?. This should simply return the string value that you stored from either of the two methods you wrote above. Use the UserDefaults string(forKey: ...) method to get that value. The method should return an optional string because if the user's theme preference hasn't been saved, the value returned from UserDefaults will be nil.
-//In the initializer for this class (you will have to call it), check if the themePreference value is nil. If it is, then call one of the setThemePreference functions you just created. This will make it so the first time the user opens the app a preference will be set for them until they choose to change it.
-//
-//Keep in mind that the implementation of this class is not the absolute best. You haven't been introduced to a few concepts that would facilitate and keep this class a bit cleaner, especially if this were to be expanded into a full theme helper class for an actual application. Don't worry though, we'll get there!
