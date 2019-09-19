b06393f937e1841336886137e00d394361f3e97b
Isvvc <lyons@tuta.io>
2019-09-19 16:58:06 -0600

    Add persistence to photo collection

--- Photo Collection/Photo Collection/PhotoController.swift
+++ Photo Collection/Photo Collection/PhotoController.swift
@@ -9,11 +9,24 @@
 import Foundation
 
 class PhotoController {
+    
+    init() {
+        loadFromPersistentStore()
+    }
+    
     var photos: [Photo] = []
+    
+    var photoListURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        return documents.appendingPathComponent("photoList.plist")
+    }
     
     func createPhoto(imageData: Data, title: String) {
         let photo = Photo(imageData: imageData, title: title)
         photos.append(photo)
+        saveToPersistentStore()
     }
     
     func updatePhoto(photo: Photo, imageData: Data, title: String) {
@@ -21,5 +34,31 @@ class PhotoController {
             let updatedPhoto = Photo(imageData: imageData, title: title)
             photos[index] = updatedPhoto
         }
+        saveToPersistentStore()
     }
+    
+    func saveToPersistentStore() {
+        guard let url = photoListURL else { return }
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let photosData = try encoder.encode(photos)
+            try photosData.write(to: url)
+        } catch {
+            print("Error saving photos data: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        do {
+            guard let url = photoListURL else { return }
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            let decodedPhotos = try decoder.decode([Photo].self, from: data)
+            photos = decodedPhotos
+        } catch {
+            print("Error loading photos data: \(error)")
+        }
+    }
+    
 }
