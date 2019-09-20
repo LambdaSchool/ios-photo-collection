92170002fb5ba482d8ae3e82d5b2c6ce76b5defa
Gi Pyo Kim <gipyok@gmail.com>
2019-09-19 22:46:16 -0700

    Added persistence for photos and titles

--- PhotoCollection/PhotoCollection/Controllers/PhotoController.swift
+++ PhotoCollection/PhotoCollection/Controllers/PhotoController.swift
@@ -12,11 +12,24 @@ class PhotoController {
     // MARK: - Properties
     var photos: [Photo] = []
     
+    private var photoCollectionURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        return documents.appendingPathComponent("PhotoCollection.plist")
+    }
+    
     
     // MARK: - Methods
+    init() {
+        loadFromPersistentStore()
+    }
+    
+    
     func create (imageData: Data, title: String) {
         let photo = Photo(imageData: imageData, title: title)
         photos.append(photo)
+        saveToPersistentStore()
     }
     
     func update (photo: Photo, imageData: Data, title: String) {
@@ -24,7 +37,35 @@ class PhotoController {
             if photos[i] == photo {
                 photos[i].imageData = imageData
                 photos[i].title = title
+                saveToPersistentStore()
             }
+        }
+    }
+    
+    func saveToPersistentStore() {
+        guard let url = photoCollectionURL else { return }
+        
+        do {
+            let encoder = PropertyListEncoder()
+            let photosData = try encoder.encode(photos)
+            try photosData.write(to: url)
+        } catch {
+            print("Error saving photo collection data: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        let fileManager = FileManager.default
+        
+        do {
+            guard let url = photoCollectionURL, fileManager.fileExists(atPath: url.path) else { return }
+            let photosData = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            let decodedPhotos = try decoder.decode([Photo].self, from: photosData)
+            photos = decodedPhotos
+            
+        } catch {
+            print("Error loading photo collection data: \(error)")
         }
     }
     
