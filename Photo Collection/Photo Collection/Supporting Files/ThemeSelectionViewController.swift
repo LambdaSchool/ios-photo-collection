f75e32fd48aed742e69d6e0ea021706eaf4147dd
Joseph Rogers <joerogersss2016@gmail.com>
2019-10-10 17:36:26 -0500

    Built Theme VC and detail VC

---
+++ Photo Collection/Photo Collection/Supporting Files/ThemeSelectionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ThemeSelectionViewController.swift
+//  Photo Collection
+//
+//  Created by Joseph Rogers on 10/10/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    //MARK: Properties
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
