d181a86771376c236a0db8f65f7db5970a3d7434
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-09 21:14:29 -0400

    created branch

---
+++ ios-photo-collection/ios-photo-collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-photo-collection
+//
+//  Created by denis cedeno on 10/9/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
