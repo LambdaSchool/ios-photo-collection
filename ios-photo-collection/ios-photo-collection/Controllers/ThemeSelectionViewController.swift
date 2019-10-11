3c9f9b3e929ee9e3fad26c12cec744dfa69a3e7b
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-11 09:29:13 -0400

    created theme selection detail  view

---
+++ ios-photo-collection/ios-photo-collection/Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ThemeSelectionViewController.swift
+//  ios-photo-collection
+//
+//  Created by denis cedeno on 10/10/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+    
+    @IBAction func selectBlueTheme(_ sender: Any) {
+    }
+    
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
