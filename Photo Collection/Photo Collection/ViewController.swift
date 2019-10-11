21d78d5d817927289deb829b12b4d23cd3657040
Gavin Murphy <gavincmurphy@gmail.com>
2019-10-10 20:30:27 -0700

    Created photo collection project, branched it, and added Photo.swift file with struct Photo

---
+++ Photo Collection/Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection
+//
+//  Created by Gavin Murphy on 10/10/19.
+//  Copyright © 2019 Gavin Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
