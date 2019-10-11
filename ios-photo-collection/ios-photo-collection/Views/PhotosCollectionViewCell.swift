faa0a9c08a19bd9b94cf12660871eb57fb00687f
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-11 09:36:06 -0400

    created update views in photo cell class

--- ios-photo-collection/ios-photo-collection/Views/PhotosCollectionViewCell.swift
+++ ios-photo-collection/ios-photo-collection/Views/PhotosCollectionViewCell.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
+    
+    
+    var photo: Photo?{
+        didSet{
+            updateViews()
+        }
+    }
+    
+    func updateViews(){
+        guard let photo = photo else {return}
+        imageView.image = UIImage(data: photo.imageData)
+        label.text = photo.title
+    }
 }
