cd8cf840430e1d371302d1abfacc1f339265cdca
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-10 23:07:38 -0400

    configured the collectionViewController Scene

---
+++ ios-photo-collection/ios-photo-collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  ios-photo-collection
+//
+//  Created by denis cedeno on 10/10/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+}
