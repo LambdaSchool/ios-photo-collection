2748b21aac595626e8b76285924d06f3a368ddf5
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-10-17 12:52:43 -0700

    Added swift files Photo, PhotoController, ThemeHelper with functions

---
+++ PhotoCollection/PhotoCollection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Lambda_School_Loaner_204 on 10/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
