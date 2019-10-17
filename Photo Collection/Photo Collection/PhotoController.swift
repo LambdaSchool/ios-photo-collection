04a20e33feb0049710e8dfcdea352c3e03c0078d
Dan M <djmthedev@gmail.com>
2019-10-17 07:04:11 -0700

    added PhotoController

---
+++ Photo Collection/Photo Collection/PhotoController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoController.swift
+//  Photo Collection
+//
+//  Created by morse on 10/17/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+class PhotoController {
+    var photos: [Photo] = []
+    
+    
+}
