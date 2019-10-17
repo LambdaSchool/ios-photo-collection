842f6e88b666ba526a8faf13a7fbef98af4ca793
Jon Bash <jonbashmusic@gmail.com>
2019-10-17 12:34:06 -0700

    init xcode project

---
+++ Photo Collection/Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection
+//
+//  Created by Jon Bash on 2019-10-17.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
