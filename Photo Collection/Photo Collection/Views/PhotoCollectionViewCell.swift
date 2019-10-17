2c247855a3b126f6e2cffa418f0947bd13220c06
Dan M <djmthedev@gmail.com>
2019-10-17 07:41:20 -0700

    set up PhotosCollectionView scene

---
+++ Photo Collection/Photo Collection/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by morse on 10/17/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
