a1922702d47b97e1abfcdd17fff2f62e50a52e63
Jon Bash <jonbashmusic@gmail.com>
2019-10-17 15:11:55 -0700

    implement update func?

--- Photo Collection/Photo Collection/Model Controllers/PhotoController.swift
+++ Photo Collection/Photo Collection/Model Controllers/PhotoController.swift
@@ -17,6 +17,8 @@ class PhotoController {
     }
     
     func update(photo: Photo, data: Data, title: String) {
-        
+        guard let index = photos.firstIndex(of: photo) else { return }
+        photos[index].imageData = data
+        photos[index].title = title
     }
 }
