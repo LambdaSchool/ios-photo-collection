f171a76c6cb89c5b43c96ff5355d7e6528727c2a
Dan M <djmthedev@gmail.com>
2019-10-17 06:48:01 -0700

    initial commit

---
+++ Photo Collection/Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection
+//
+//  Created by morse on 10/17/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
