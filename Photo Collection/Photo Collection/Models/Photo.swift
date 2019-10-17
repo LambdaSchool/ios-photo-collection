71f59fae43a71fa961650dad53395ec0e9197533
Jon Bash <jonbashmusic@gmail.com>
2019-10-17 12:44:59 -0700

    add photo model

---
+++ Photo Collection/Photo Collection/Models/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Jon Bash on 2019-10-17.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
