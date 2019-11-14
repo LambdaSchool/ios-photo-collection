067e39849a31999f0ff11f9e8e7219424ed2da1b
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-14 12:58:25 -0800

    Created theme preference and property keys in ThemeHelper.swift file

--- Photo Collection/Photo Collection/ThemeHelper.swift
+++ Photo Collection/Photo Collection/ThemeHelper.swift
@@ -10,4 +10,31 @@ import Foundation
 
 class ThemeHelper {
 
+
+    var themePreferenceKey: String? {
+        return UserDefaults.standard.string(forKey: .themePreference)
+    }
+    
+    init() {
+        if themePreferenceKey == nil {
+            setThemePreferenceToYourColorHere()
+        }
+    }
+    
+    struct PropertyKeys {
+        static let dark = "Dark"
+        static let blue = "Blue"
+    }
+    
+    func setThemePreferenceToDark() {
+        UserDefaults.standard.set(PropertyKeys.dark, forKey: .themePreference)
+    }
+    
+    func setThemePreferenceToYourColorHere() {
+        UserDefaults.standard.set(PropertyKeys.blue, forKey: .themePreference)
+    }
+}
+
+extension String {
+    static var themePreference = "themePreference"
 }
