e8b747242e43f2578ea10b51e06458bb8a32c823
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-14 14:44:38 -0800

    Added two scenes to main.storyboard; one is to view the photos in a collection view and the other is to add a photo/title.

---
+++ Photo Collection/Photo Collection/PhotoDetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PhotoDetailViewController.swift
+//  Photo Collection
+//
+//  Created by Donella Barcelo on 11/14/19.
+//  Copyright © 2019 Donella Barcelo. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    
+    @IBOutlet weak var addPhotoImageView: UIImageView!
+    @IBOutlet weak var addTitleTextField: UITextField!
+    
+    
+    
+    
+    
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addPhoto(_ sender: Any) {
+    }
+    @IBAction func savePhoto(_ sender: Any) {
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Donella Barcelo on 11/14/19.
+//  Copyright © 2019 Donella Barcelo. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photosImageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+    
+    
+}
