f8ff56557f9c56904d7a5077cb847894f8d9656f
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-14 15:19:10 -0800

    Wired up and implemented photo collection view cell

--- Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -13,5 +13,16 @@ class PhotosCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var photosImageView: UIImageView!
     @IBOutlet weak var photoLabel: UILabel!
     
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
     
+    func updateViews() {
+        guard let photo = photo else { return }
+        
+        photoLabel.text = photo.title
+        photosImageView.image = UIImage(data: photo.imageData)
+    }
 }
