b7348db56571591c895479323151777711526948
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-14 14:58:51 -0500

    Added 'Photo' struct

---
+++ PhotoCollection/PhotoCollection/Controller/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Chad Rutherford on 11/14/19.
+//  Copyright © 2019 Chad Rutherford. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
