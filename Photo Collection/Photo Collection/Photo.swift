4f4689523b023e4d06459ad52b26fb667f371d47
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-14 12:16:44 -0800

    Initial commit. Created xcode project and added Photo.swift file.

---
+++ Photo Collection/Photo Collection/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Donella Barcelo on 11/14/19.
+//  Copyright © 2019 Donella Barcelo. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    
+    var imageData: Data
+    var title: String
+}
