4f01921a074d674b2ea3317e24f5b86298235694
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-12-12 15:35:56 -0500

    Implemented PhotoController

---
+++ PhotoCollection/PhotoCollection/Model Controller/PhotoController.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoController.swift
+//  PhotoCollection
+//
+//  Created by Joshua Rutkowski on 12/12/19.
+//  Copyright © 2019 Joshua Rutkowski. All rights reserved.
+//
+
+import Foundation
+
+class PhotoController {
+    var photos: Photo = []
+    
+    func createPhoto(named title: String, with data: Data) {
+        let photo = Photo(title: title, imageData: data)
+        photos.append(photo)
+    }
+    
+    func updatePhoto(photo: Photo, named title: String, with data: Data) {
+        guard let index = photos.firstIndex(of: Photo) else { return }
+        let photo = Photo(title: title, imageData: data)
+        photos[index] = photo
+        
+    }
+}
