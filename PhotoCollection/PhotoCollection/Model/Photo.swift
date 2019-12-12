015ffbd18847fa00831437817bac540a91228674
David <davidtwright89@gmail.com>
2019-12-12 01:15:49 -0500

    Set up model layer.

---
+++ PhotoCollection/PhotoCollection/Model/Photo.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by David Wright on 12/12/19.
+//  Copyright © 2019 David Wright. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    
+    var imageData: Data
+    var title: String
+    
+}
