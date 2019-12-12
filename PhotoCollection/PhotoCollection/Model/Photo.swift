75f26894e83e839ba23f4babb838c54748754c7a
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-12-12 15:23:30 -0500

    Set up Photo struct

---
+++ PhotoCollection/PhotoCollection/Model/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Joshua Rutkowski on 12/12/19.
+//  Copyright © 2019 Joshua Rutkowski. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
