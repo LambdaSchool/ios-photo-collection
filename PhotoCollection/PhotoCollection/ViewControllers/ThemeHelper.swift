fb3a34f43dc2821baef0e29ed107811cdc385036
Christopher Price <christopher.r.price@gmail.com>
2019-12-12 15:05:22 -0500

    Tried to write the ThemeHelper.swift file, but languished.

--- PhotoCollection/PhotoCollection/ViewControllers/ThemeHelper.swift
+++ PhotoCollection/PhotoCollection/ViewControllers/ThemeHelper.swift
@@ -12,4 +12,15 @@ class ThemeHelper {
     let themePreferenceKey: String
     
     func setThemePreferenceToDark() {
+        UserDefaults.standard.set("Dark", forKey: themePreferenceKey)
+    }
+    
+    func setThemePreferenceToBlue() {
+        UserDefaults.standard.set("Blue", forKey: themePreferenceKey)
+    }
+    
+    var themePreference: String? {
+        if // one case for dark, one case for blue, default case set to the dark result.
+        //This way dark will be the default, but it can also be selected if Blue had previously been selected.
+    }
 }
