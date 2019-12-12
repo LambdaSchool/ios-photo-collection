7df6999d7e3b5c15b7b20722f59288bc99ca13d0
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-12-12 15:10:53 -0500

    Create photocollection
    Create photocollection

---
+++ PhotoCollection/PhotoCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Joshua Rutkowski on 12/12/19.
+//  Copyright © 2019 Joshua Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
