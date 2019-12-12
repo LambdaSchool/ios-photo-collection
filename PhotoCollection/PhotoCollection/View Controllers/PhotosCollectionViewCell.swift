8e4c558f91894160fa2c685d48d7f92ce1efa51a
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-12-12 18:52:02 -0500

    Set up CollectionViewController Scene

---
+++ PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Joshua Rutkowski on 12/12/19.
+//  Copyright © 2019 Joshua Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
