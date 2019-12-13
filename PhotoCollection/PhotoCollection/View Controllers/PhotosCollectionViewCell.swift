995e3a10c77f3b64c133367baf8e860846e9d653
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-12-12 21:01:43 -0500

    Set up variable and updateViews() function in PhotoCollectionViewCell

--- PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewCell.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class PhotosCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+    
+    var photo: Photo? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        guard let photo = photo else { return }
+        photoLabel.text = photo.title
+        photoImageView.image = UIImage(data: photo.imageData)
+    }
 }
