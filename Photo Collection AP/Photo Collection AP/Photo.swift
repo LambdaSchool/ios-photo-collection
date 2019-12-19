494ebea2b20ad83ba35a66c5839832c94279e549
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-19 12:16:11 -0800

    finished Part 1 MLSU

---
+++ Photo Collection AP/Photo Collection AP/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection AP
+//
+//  Created by Jorge Alvarez on 12/19/19.
+//  Copyright © 2019 Jorge Alvarez. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
