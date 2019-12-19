6547cf087143738844321fbd67581d769a2900db
Kenny Dubroff <kennydubroff@hazystudios.com>
2019-12-19 14:59:02 -0800

    Added Main View and CocoaTouch Class

---
+++ Photo Collection/Photo Collection/Controller/View Controller/PhotoDetailController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoDetailController.swift
+//  Photo Collection
+//
+//  Created by Kenny on 12/19/19.
+//  Copyright © 2019 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailController: UIViewController {
+
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Photo Collection/Photo Collection/View/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Kenny on 12/19/19.
+//  Copyright © 2019 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+}
