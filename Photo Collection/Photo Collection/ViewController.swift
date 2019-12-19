834090a51e075bf4ffba082c41bfc3d44506b416
Kenny Dubroff <kennydubroff@hazystudios.com>
2019-12-19 13:22:07 -0800

    Initial commit

---
+++ Photo Collection/Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection
+//
+//  Created by Kenny on 12/19/19.
+//  Copyright © 2019 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
