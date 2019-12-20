3edc17b770464611618c65cac9be296e39718d40
Malik <malik21barnes@gmail.com>
2019-12-20 09:02:53 -0800

    First commit

---
+++ Photo Collection/Photo Collection/Swift Files/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Malik Barnes on 12/19/19.
+//  Copyright © 2019 Malik Barnes. All rights reserved.
+//
+
+import Foundation
+
+struct photo {
+    var imageData: Data
+    var title: String
+}
+
