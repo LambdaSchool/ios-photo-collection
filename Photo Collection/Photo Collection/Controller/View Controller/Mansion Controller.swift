a7411bdaa75a450c1c0902f617acb7e3917e94a9
Kenny Dubroff <kennydubroff@hazystudios.com>
2020-01-10 16:08:54 -0800

    commit to track master repo

---
+++ Photo Collection/Photo Collection/Controller/View Controller/Mansion Controller.swift
@@ -0,0 +1,9 @@
+//
+//  Mansion Controller.swift
+//  iOS13-CollectionView
+//
+//  Created by Kenny on 12/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Photo Collection/Photo Collection/Controller/View Controller/MansionController.swift
@@ -0,0 +1,9 @@
+//
+//  MansionController.swift
+//  iOS13-CollectionView
+//
+//  Created by Kenny on 12/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
