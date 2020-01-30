79abb8425c5575f276f4a4ebbc09d1bf1a5e68eb
scottharris86 <scottharrisdev@gmail.com>
2020-01-30 15:50:17 -0500

    initial commit

---
+++ Photos/Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by scott harris on 1/30/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
