8601bcd32c8455293b462f4703579be1b5ce6af0
Jarren Campos <jarrenmcampos@gmail.com>
2020-02-19 20:49:48 -0800

    Project Creation

---
+++ iosPhotoApp/iosPhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosPhotoApp
+//
+//  Created by Jarren Campos on 2/19/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
