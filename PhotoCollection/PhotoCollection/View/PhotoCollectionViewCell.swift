1c570bfcf8aef1fd01ec61c257d4bca859d5dae0
Claudia Maciel <clc_80@yahoo.com>
2020-02-20 11:00:45 -0800

    Storyboard for Collection View completed

---
+++ PhotoCollection/PhotoCollection/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Claudia Contreras on 2/20/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: - IBOutlets
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var photoTitleLabel: UILabel!
+    
+    
+}
