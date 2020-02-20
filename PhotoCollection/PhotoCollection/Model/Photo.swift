3aadff782a2ee468c51d0887bb2bd52e6e596e28
Claudia Maciel <clc_80@yahoo.com>
2020-02-20 10:33:51 -0800

    Photo model created

---
+++ PhotoCollection/PhotoCollection/Model/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Claudia Contreras on 2/20/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+    
+}
