38d08d5306c21c9749cab35eb307db8ea93e0ac5
Claudia Maciel <clc_80@yahoo.com>
2020-02-20 11:29:15 -0800

    ThemeSelectionViewController created

---
+++ PhotoCollection/PhotoCollection/View Controller/ThemeSelectionViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Claudia Contreras on 2/20/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    //MARK: - Properties
+    var themeHelper: ThemeHelper?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - IBAction
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    
+    @IBAction func selectBlueTheme(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
