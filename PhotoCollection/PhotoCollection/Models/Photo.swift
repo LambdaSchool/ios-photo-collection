e4e66ceb1dc9ebabd1efcc0cc437bf1fd21b8344
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-20 23:38:22 -0500

    Initial commit

---
+++ PhotoCollection/PhotoCollection/Models/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Lambda_School_Loaner_259 on 2/19/20.
+//  Copyright © 2020 DeVitoC. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
