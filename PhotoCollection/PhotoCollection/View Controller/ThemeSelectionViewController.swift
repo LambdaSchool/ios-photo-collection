5eac6b2ccc751d4e0a5ce72478d672e713d449c3
Claudia Maciel <clc_80@yahoo.com>
2020-02-20 17:30:45 -0800

    Theme SelectionViewController coded

--- PhotoCollection/PhotoCollection/View Controller/ThemeSelectionViewController.swift
+++ PhotoCollection/PhotoCollection/View Controller/ThemeSelectionViewController.swift
@@ -16,25 +16,19 @@ class ThemeSelectionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
     //MARK: - IBAction
     @IBAction func selectDarkTheme(_ sender: Any) {
+        themeHelper?.setThemePreferenceToDark()
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func selectBlueTheme(_ sender: Any) {
+        themeHelper?.setThemePreferenceToBlue()
+        dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
