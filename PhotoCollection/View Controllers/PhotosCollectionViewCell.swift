fe1fbafdccccbf165393ed55f2263cd997ec04df
Chad Parker <chad@chadparker.com>
2020-02-20 20:13:00 -0800

    Set up PhotosCollectionView and Cell

---
+++ PhotoCollection/View Controllers/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Chad Parker on 2/20/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
