fb0d6ecf5af4664a7966bfb219fdd8a079aaa7e9
kensam1 <samarakodykennedy@gmail.com>
2020-02-20 23:30:15 -0500

    Photo Collection Project

---
+++ PhotoCollection/PhotoCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Lambda_School_Loaner_241 on 2/20/20.
+//  Copyright © 2020 Lambda_School_Loaner_241. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
