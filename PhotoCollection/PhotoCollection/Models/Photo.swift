4bbe461b1d67eebbaba8419c54aff7bd6c17dd8f
Hector L <hectoremil21@hotmail.com>
2020-02-26 12:27:04 -0500

    Photo model

---
+++ PhotoCollection/PhotoCollection/Models/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Karen Rodriguez on 2/26/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
