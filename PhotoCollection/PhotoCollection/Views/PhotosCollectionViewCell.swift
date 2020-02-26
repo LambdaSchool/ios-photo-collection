0fd5a006e8068665ff8e2c78524cbe0441983b07
Hector L <hectoremil21@hotmail.com>
2020-02-26 13:20:52 -0500

    Create Cell controller. Insert label and text view and create outlets.

---
+++ PhotoCollection/PhotoCollection/Views/PhotosCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Karen Rodriguez on 2/26/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
