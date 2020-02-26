d25fcbf72eccc0fa28ca841e1b0898ffdbf98940
Hector L <hectoremil21@hotmail.com>
2020-02-26 13:57:25 -0500

    PhotoDetail View controller create. Add variables.

---
+++ PhotoCollection/PhotoCollection/Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollection
+//
+//  Created by Karen Rodriguez on 2/26/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
