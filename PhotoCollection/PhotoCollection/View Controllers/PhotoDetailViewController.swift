9049f6a1fe83be5b7f3093fe3c60d8faa821b497
Nichole Davidson <nicholedavid@gmail.com>
2020-02-27 15:06:58 -0700

    Setup PhotoDetailVC and scene and wired up outlets and actions

---
+++ PhotoCollection/PhotoCollection/View Controllers/PhotoDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollection
+//
+//  Created by Nichole Davidson on 2/27/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    @IBOutlet weak var addImage: UIImageView!
+    @IBOutlet weak var addPhotoTitle: UITextField!
+    
+    
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+    }
+    
+    @IBAction func savePhoto(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
