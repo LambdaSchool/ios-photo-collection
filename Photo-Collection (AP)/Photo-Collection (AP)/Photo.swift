6bef3decc7eea65d8fd2a62cc82531f034115cf8
Samuel Esserman <samessermanlambda@gmail.com>
2020-02-27 14:17:17 -0500

    Completed Photo.swift file and struct

---
+++ Photo-Collection (AP)/Photo-Collection (AP)/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo-Collection (AP)
+//
+//  Created by Samuel Esserman on 2/27/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String?
+}
