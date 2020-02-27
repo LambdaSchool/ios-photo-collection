042cf4f36b378ad776fc31812eac30d5580303e8
Mark Gerrior <github@gerrior.com>
2020-02-27 13:12:30 -0800

    Added Photo object

---
+++ Photo Collection/Photo Collection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Mark Gerrior on 2/27/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
