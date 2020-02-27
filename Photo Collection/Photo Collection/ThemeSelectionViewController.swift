4b91496cf5a02e45ec1ecd32b2f541bc9417f71b
Mark Gerrior <github@gerrior.com>
2020-02-27 15:45:23 -0800

    ThemeSelectionViewController Implementation

--- Photo Collection/Photo Collection/ThemeSelectionViewController.swift
+++ Photo Collection/Photo Collection/ThemeSelectionViewController.swift
@@ -24,18 +24,8 @@ class ThemeSelectionViewController: UIViewController {
     }
     
     @IBAction func blueButton(_ sender: Any) {
+        //FIXME: actually change the color!
         themeHelper?.setThemePreferenceToBlue()
         navigationController?.popViewController(animated: true)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
