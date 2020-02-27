050403f12005d52746e8f52e42719edb2ff2c1cb
Nichole Davidson <nicholedavid@gmail.com>
2020-02-27 15:21:38 -0700

    Setup ThemeVC scene and file and wired up labels

---
+++ PhotoCollection/PhotoCollection/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Nichole Davidson on 2/27/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    
+    var themeHelper: ThemeHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    
+    
+    @IBAction func selectYourColorHereTheme(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
