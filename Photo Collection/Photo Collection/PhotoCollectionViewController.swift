de19bd06d11e5b81bd98378f75c2b1a78edf3380
Wyatt Harrell <wyattaharrell@gmail.com>
2020-02-27 17:51:23 -0500

    done

--- Photo Collection/Photo Collection/PhotoCollectionViewController.swift
+++ Photo Collection/Photo Collection/PhotoCollectionViewController.swift
@@ -86,3 +86,20 @@ class PhotoCollectionViewController: UICollectionViewController {
     }
 
 }
+
+/*
+extension PhotoCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+                return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+
+    }
+}
+*/
