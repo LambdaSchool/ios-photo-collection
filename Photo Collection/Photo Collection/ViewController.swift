8faa7c1f49559357309237bc6d149165b672c347
Mark Gerrior <github@gerrior.com>
2020-02-27 13:03:21 -0800

    Initial project

---
+++ Photo Collection/Photo Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Collection
+//
+//  Created by Mark Gerrior on 2/27/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
