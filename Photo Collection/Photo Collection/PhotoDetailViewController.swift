386447b6bc8deb11d7b40870b959877034657939
Mark Gerrior <github@gerrior.com>
2020-02-27 14:36:51 -0800

    Added PhotoDetailViewController Scene

---
+++ Photo Collection/Photo Collection/PhotoDetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoDetailViewController.swift
+//  Photo Collection
+//
+//  Created by Mark Gerrior on 2/27/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var theTextField: UITextField!
+  
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
