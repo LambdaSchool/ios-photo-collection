3b1eefa09fa7ef694b4848fa61cef7ed55c4faa0
Samuel Esserman <samessermanlambda@gmail.com>
2020-02-27 15:13:19 -0500

    Completed CollectionVC Scene

---
+++ Photo-Collection (AP)/Photo-Collection (AP)/PhotosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo-Collection (AP)
+//
+//  Created by Samuel Esserman on 2/27/20.
+//  Copyright © 2020 Samuel Esserman. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
