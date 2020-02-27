8618a0c6d7119c549310c75bb94b6e692007b42d
Nichole Davidson <nicholedavid@gmail.com>
2020-02-27 14:48:17 -0700

    VC scene initial setup

---
+++ PhotoCollection/PhotoCollection/View Controllers/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Nichole Davidson on 2/27/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
