34bfd15b13bd5194cfac306abc1e5fea962d7fb7
Nichole Davidson <nicholedavid@gmail.com>
2020-02-27 14:21:39 -0700

    Added themeHelper

---
+++ PhotoCollection/PhotoCollection/Model Controller/ThemeHelper.swift
@@ -0,0 +1,36 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by Nichole Davidson on 2/27/20.
+//  Copyright © 2020 Nichole Davidson. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    // Use this whenever you need to save user's theme preference or get it from UserDefaults
+    let themePreferenceKey: String
+    
+    func setThemePreferenceToDark() {
+        UserDefaults.standard.set("Dark", forKey: themePreferenceKey)
+    }
+    
+    func setThemePreferenceToYourColorHere() {
+        UserDefaults.standard.set("Burnt Sienna", forKey: themePreferenceKey)
+    }
+    
+    var themePreference: String? {
+        UserDefaults.standard.string(forKey: themePreferenceKey)
+        return themePreferenceKey
+    }
+    
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToYourColorHere()
+        } else {
+            setThemePreferenceToDark()
+        }
+        init()
+    }
+}
