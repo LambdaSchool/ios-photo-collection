23ff33b435ce24633ba879a3d19989058dfaa31a
Mark Gerrior <github@gerrior.com>
2020-02-27 21:32:51 -0800

    Flushed out PhotoController.update

--- Photo Collection/Photo Collection/PhotoController.swift
+++ Photo Collection/Photo Collection/PhotoController.swift
@@ -13,15 +13,37 @@ class PhotoController {
     
     func create(pic: Photo) {
         photos.append(pic)
+
+        // FIXME: Save
+        print("Save")
     }
 
     func create(title: String, image: Data ) {
         let pic = Photo(imageData: image, title: title)
         
         photos.append(pic)
+
+        // FIXME: Save
+        print("Save")
     }
     
     func update(pic: Photo, title: String, image: Data) {
+        var somethingChanged = false
         
+        if let index = photos.firstIndex(where: { $0 == pic }) {
+            if (photos[index].title != title) {
+                photos[index].title = title
+                somethingChanged = true
+            }
+            if (photos[index].imageData != image) {
+                photos[index].imageData = image
+                somethingChanged = true
+            }
+        }
+        
+        if somethingChanged {
+            // FIXME: Save
+            print("Save")
+        }
     }
 }
