4deddab991ad47420b4bbcbcb9590709476b759d
Mark Gerrior <github@gerrior.com>
2020-02-27 22:42:42 -0800

    Stretch: add persistence

--- Photo Collection/Photo Collection/PhotoController.swift
+++ Photo Collection/Photo Collection/PhotoController.swift
@@ -11,20 +11,23 @@ import Foundation
 class PhotoController {
     var photos = [Photo]()
     
+    // This initilizer is treated as the viewDidLoad of the model controller.
+    init() {
+        loadFromPersistentStore()
+    }
+
     func create(pic: Photo) {
         photos.append(pic)
 
-        // FIXME: Save
-        print("Save")
+        saveToPersistentStore()
     }
 
     func create(title: String, image: Data ) {
         let pic = Photo(imageData: image, title: title)
         
         photos.append(pic)
-
-        // FIXME: Save
-        print("Save")
+        
+        saveToPersistentStore()
     }
     
     func update(pic: Photo, title: String, image: Data) {
@@ -42,8 +45,52 @@ class PhotoController {
         }
         
         if somethingChanged {
-            // FIXME: Save
-            print("Save")
+            saveToPersistentStore()
+        }
+    }
+
+    // MARK: Persistent Store
+    
+    var photosURL: URL? {
+        let fileManager = FileManager.default
+        
+        let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        let photosUrl = documentsDir?.appendingPathComponent("photos.plist")
+        
+        return photosUrl
+    }
+    
+    func saveToPersistentStore() {
+        // Convert our book Property List
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let photosData = try encoder.encode(photos)
+            
+            guard let photosUrl = photosURL else { return }
+            
+            try photosData.write(to: photosUrl)
+            
+        } catch {
+            print("Unable to save photos to plist: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        
+        do {
+            guard let photosURL = photosURL else { return }
+            
+            let photosData = try Data(contentsOf: photosURL)
+            
+            let decoder = PropertyListDecoder()
+            
+            let decodedPhotos = try decoder.decode([Photo].self, from: photosData)
+            
+            self.photos = decodedPhotos
+        } catch {
+            print("Unable to open photos plist: \(error)")
         }
     }
 }
