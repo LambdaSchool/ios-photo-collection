b5c8a5efb969168cf4b763f6f6dd2d8c98df1749
Cody Morley <codymorleyinc@gmail.com>
2020-03-26 13:37:53 -0700

    finished themeHelper

---
+++ Photo Collection/Photo Collection/Model/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Cody Morley on 3/26/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
