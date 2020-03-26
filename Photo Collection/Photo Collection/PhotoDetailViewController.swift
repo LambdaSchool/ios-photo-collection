44f6943c8cfbed34b7412fe04732de958dd5cc51
Harmony Radley <harmonyradley99@gmail.com>
2020-03-26 17:46:09 -0400

    Created ViewController for PhotoDetailVC. Added objects and set constraints

---
+++ Photo Collection/Photo Collection/PhotoDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoDetailViewController.swift
+//  Photo Collection
+//
+//  Created by Harmony Radley on 3/26/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    var photoController : PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Photo Collection/Photo Collection/ThemeSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThemeSelectionViewController.swift
+//  Photo Collection
+//
+//  Created by Harmony Radley on 3/26/20.
+//  Copyright © 2020 Harmony Radley. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
