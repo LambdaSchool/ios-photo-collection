63517c078c352e0a9668c593ade6c6a1630da992
Cody Morley <codymorleyinc@gmail.com>
2020-03-26 13:56:01 -0700

    finished setting up Collection VC

---
+++ Photo Collection/Photo Collection/Views/PhotosCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Cody Morley on 3/26/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+   
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
+    
+    
+    
+    
+}
