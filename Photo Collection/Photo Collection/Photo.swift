c3e5e6691ec33ade56a545d1d15e6c78879bc16d
Chris Dobek <dobek.christopher@gmail.com>
2020-03-26 16:08:18 -0400

    Part 1 Finished

---
+++ Photo Collection/Photo Collection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  Photo Collection
+//
+//  Created by Chris Dobek on 3/26/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    let imageData: Data
+    let title: String
+}
---
+++ Photo Collection/Photo Collection/PhotoController.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoController.swift
+//  Photo Collection
+//
+//  Created by Chris Dobek on 3/26/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+
+class PhotoController {
+    var photos: [Photo] = []
+    
+    
+    func create() {
+        
+    }
+    
+    func update() {
+        
+    }
+}
