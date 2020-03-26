d5b0ea4b8665d3acff0418b423f2025e03c908c3
Chris Dobek <dobek.christopher@gmail.com>
2020-03-26 16:50:55 -0400

    PhotoCollectionVC Finished

---
+++ Photo Collection/Photo Collection/PhotosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Photo Collection
+//
+//  Created by Chris Dobek on 3/26/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
