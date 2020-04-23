8d9deb9b7bc677013e1354ce8c0a8fab1b5de853
violet-love <violetlove9419@icloud.com>
2020-04-23 13:44:51 -0700

    createed photo struct, started photo controller class, struggling with CRUD

---
+++ PhotoCollection/PhotoCollection/Model Controller/PhotoController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoController.swift
+//  PhotoCollection
+//
+//  Created by Violet Lavender Love on 4/23/20.
+//  Copyright © 2020 Love. All rights reserved.
+//
+
+import Foundation
+
+class PhotoController {
+    
+    var photos: [Photo] = []
+    photos.append(Photo())
+    
+    
+    
+}
---
+++ PhotoCollection/PhotoCollection/photo.swift
@@ -0,0 +1,17 @@
+//
+//  photo.swift
+//  PhotoCollection
+//
+//  Created by Violet Lavender Love on 4/23/20.
+//  Copyright © 2020 Love. All rights reserved.
+//
+
+import Foundation
+
+
+struct Photo: Equatable {
+    
+    var imageData: Data
+    var title: String
+    
+}
