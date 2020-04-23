d5d2e7ce197bcd62e8d6a40d650d0748d52dc100
violet-love <violetlove9419@icloud.com>
2020-04-23 14:49:20 -0700

    What the heck is a helper class?

---
+++ PhotoCollection/ThemeHelper.swift
@@ -0,0 +1,21 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by Violet Lavender Love on 4/23/20.
+//  Copyright © 2020 Love. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    
+    let themePreferenceKey: String
+
+    func setThemePreferenceToDark() {
+        set
+    }
+    
+    
+    
+}
