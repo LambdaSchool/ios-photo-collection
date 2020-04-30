46f7950dac68b4f9b0c1008697a5a00c615b052c
Clayton Watkins <watkinclayton17@yahoo.com>
2020-04-29 21:29:07 -0700

    Added Theme selection, finished Part 3

---
+++ PhotoCollectionView/PhotoCollectionView/View Controllers/ThemeSelectionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollectionView
+//
+//  Created by Clayton Watkins on 4/29/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    //MARK: - Properties
+    var themeHelper: ThemeHelper?
+    
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - IBActions
+    @IBAction func selectDarkTheme(_ sender: Any) {
+        
+    }
+    
+    @IBAction func selectYourColorHereTheme(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
