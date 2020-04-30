60309fce1ae4395f6bfb9d571652378800103142
Your Name Here <youremailhere>
2020-04-30 00:07:16 -0400

    Added initial Swift files from Part 1

---
+++ photoCollection/photoCollection/Model/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  photoCollection
+//
+//  Created by Sammy Alvarado on 4/29/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
---
+++ photoCollection/photoCollection/Model/PhotoController.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoController.swift
+//  photoCollection
+//
+//  Created by Sammy Alvarado on 4/30/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import Foundation
+
+class PhotoController {
+    var photos: [Photo] = []
+    
+    init(photos: [Photo]) {
+        self.photos = photos
+    }
+    
+    func update(photo: Photo, data: Data, string: String) {
+        
+    }
+}
