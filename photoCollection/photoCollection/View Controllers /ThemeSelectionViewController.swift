269be1c16a65ede128441c43f44874455752241b
Your Name Here <youremailhere>
2020-05-01 00:03:10 -0400

    Added ThemeSelectionViewController Scene

--- photoCollection/photoCollection/View Controllers /ThemeSelectionViewController.swift
+++ photoCollection/photoCollection/View Controllers /ThemeSelectionViewController.swift
@@ -18,6 +18,14 @@ class ThemeSelectionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func selectDarkTheme(_ sender: Any) {
+    }
+    
+    @IBAction func selectYourColorHereTheme(_ sender: Any) {
+    }
+    
+    
 
     /*
     // MARK: - Navigation
