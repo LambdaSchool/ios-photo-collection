5dd4843f8a98813d4dbfc62fd388ccca6a249a5e
Bronson <bronsonmullens@icloud.com>
2020-04-30 17:58:17 -0700

    Completed Theme VC

--- Photo Collection/Photo Collection/View Controllers/ThemeSelectionViewController.swift
+++ Photo Collection/Photo Collection/View Controllers/ThemeSelectionViewController.swift
@@ -12,9 +12,13 @@ class ThemeSelectionViewController: UIViewController {
     
     // MARK: - IBActions
     @IBAction func selectDarkTheme(_ sender: UIButton) {
+        themeHelper?.setThemePreferenceToDark()
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func selectBlueTheme(_ sender: UIButton) {
+        themeHelper?.setThemePreferenceToBlue()
+        dismiss(animated: true, completion: nil)
     }
     
     var themeHelper: ThemeHelper?
@@ -22,16 +26,5 @@ class ThemeSelectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
