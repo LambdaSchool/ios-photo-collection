0bddb5eef02e000c0a9d01f086031793fa1e40d3
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-21 16:36:08 -0700

    added constraints to label and button on selection view controller

---
+++ PhotoCollection/PhotoCollection/ViewControllers/ThemeSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThemeSelectionViewController.swift
+//  PhotoCollection
+//
+//  Created by Richard Gibbs on 5/21/20.
+//  Copyright © 2020 Rich Gibbs. All rights reserved.
+//
+
+import UIKit
+
+class ThemeSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
