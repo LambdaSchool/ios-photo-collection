12cab609665ee7ea2b462a2014e3b8daa6269f1a
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-21 15:45:36 -0700

    added bar button items for theme and adding new photos

---
+++ PhotoCollection/PhotoCollection/Storyboards/PhotosCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by Richard Gibbs on 5/21/20.
+//  Copyright © 2020 Rich Gibbs. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+    
+}
