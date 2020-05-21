d751d3627daaff8ee9dcff78a9c308c44906d400
Ian Becker <ian-becker@lambdastudents.com>
2020-05-21 13:22:40 -0700

    Made photo struct, photo controller class, and theme helper class

---
+++ PhotoCollection/PhotoCollection/PhotoController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoController.swift
+//  PhotoCollection
+//
+//  Created by Ian Becker on 5/21/20.
+//  Copyright © 2020 Ian Becker. All rights reserved.
+//
+
+import Foundation
+
+class PhotoController {
+    var photos: [Photo] = []
+    
+    func Create(imageData: Data, title: String) {
+        let photo = Photo(imageData: imageData, title: title)
+        
+        photos.append(photo)
+    }
+    
+    func Update(photo: Photo, imageData: Data, title: String) {
+    }
+}
+
+
