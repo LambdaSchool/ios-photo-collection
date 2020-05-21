58678c514fca762f38dcaad94c820dd9f8587193
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-21 16:14:46 -0700

    added constraints to my PhotoDetailViewController

---
+++ PhotoCollection/PhotoCollection/PhotoDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoDetailViewController.swift
+//  PhotoCollection
+//
+//  Created by Richard Gibbs on 5/21/20.
+//  Copyright © 2020 Rich Gibbs. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    
+    var photoController: PhotoController?
+    var photo: Photo?
+    var themeHelper: ThemeHelper?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
