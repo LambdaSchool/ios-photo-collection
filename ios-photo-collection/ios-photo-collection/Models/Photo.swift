f2e341a0225ecdb5f252f2b725e9653091850451
Bryan Cress <8675309+cressbj@users.noreply.github.com>
2020-05-22 09:51:29 -0400

    Part 1 Model Layer Set Up: Photo

---
+++ ios-photo-collection/ios-photo-collection/Models/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  ios-photo-collection
+//
+//  Created by Bryan Cress on 5/22/20.
+//  Copyright © 2020 Bryan Cress. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
