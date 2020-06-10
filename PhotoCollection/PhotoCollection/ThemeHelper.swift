ccb81f2ba27300feb9db21a394ca6f49b2b571b7
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-06-10 10:27:30 -0700

    Part 2

---
+++ PhotoCollection/PhotoCollection/ThemeHelper.swift
@@ -0,0 +1,36 @@
+//
+//  ThemeHelper.swift
+//  PhotoCollection
+//
+//  Created by Cora Jacobson on 6/10/20.
+//  Copyright © 2020 Cora Jacobson. All rights reserved.
+//
+
+import Foundation
+
+class ThemeHelper {
+    let themePreferenceKey = "themePreference"
+    
+    func setThemePreferenceToDark() {
+        let themePreference = "Dark"
+        UserDefaults.standard.set(themePreference, forKey: themePreferenceKey)
+    }
+    
+    func setThemePreferenceToPurple() {
+        let themePreference = "Purple"
+        UserDefaults.standard.set(themePreference, forKey: themePreferenceKey)
+    }
+    
+    var themePreference: String? {
+        if let theme = UserDefaults.standard.string(forKey: themePreferenceKey) {
+            return theme
+        }
+    }
+    
+    init() {
+        if themePreference == nil {
+            setThemePreferenceToPurple()
+        }
+    }
+    
+}
